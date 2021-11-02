e17f20ca5bacab6c49806b6c05858560d6435e97
Joakim Henriksson <joakim.henriksson@ftrack.com>
2021-11-02 16:40:27 +0100

    fix window

--- source/socket.io-websocket-only.js
+++ source/socket.io-websocket-only.js
@@ -1,5 +1,5 @@
 /*! Socket.IO.js build:0.9.17, development. Copyright(c) 2011 LearnBoost <dev@learnboost.com> MIT Licensed */
-const that = window || {};
+const that = typeof window === "undefined" ? {} : window;
 
 var io = "undefined" === typeof module ? {} : module.exports;
 (function () {
