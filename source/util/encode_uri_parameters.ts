4ae62d77bbe1d0125aa3febec52065d55dc3ff25
Jimmy Callin <jimmy.callin@gmail.com>
2023-02-05 12:12:49 +0100

    build: migrate encode_uri_parameters to ts

--- source/util/encode_uri_parameters.ts
+++ source/util/encode_uri_parameters.ts
@@ -1,6 +1,6 @@
 // :copyright: Copyright (c) 2018 ftrack
 
-function encodeUriParameters(data) {
+function encodeUriParameters(data: { [key: string]: string | number }): string {
   return Object.keys(data)
     .map((key) => [key, data[key]].map(encodeURIComponent).join("="))
     .join("&");
