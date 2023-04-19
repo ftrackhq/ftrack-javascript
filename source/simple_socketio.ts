0f4e38dee18e1df33d9dba113d45a619cdacc23b
Lars Johansson <lars.johansson@ftrack.com>
2023-04-19 11:53:17 +0200

    Add tsdoc comments

--- source/simple_socketio.ts
+++ source/simple_socketio.ts
@@ -21,6 +21,25 @@ interface Payload {
   args: Event["_data"][];
 }
 
+/**
+ * SimpleSocketIOClient is a lightweight WebSocket client that simplifies communication
+ * with a server using a simplified version of the Socket.IO 0.9 protocol. It handles connection,
+ * event emission, event handling, and automatic reconnection.
+ *
+ * Example usage:
+ * ```
+ * const client = new SimpleSocketIOClient(serverUrl, apiUser, apiKey);
+ *
+ * client.on("connect", () => {
+ *   console.log("Connected!");
+ *   client.emit("my-event", { data: "Hello, world!" });
+ * });
+ *
+ * client.on("my-event", (eventData) => {
+ *   console.log("Received response:", eventData);
+ * });
+ * ```
+ */
 export default class SimpleSocketIOClient {
   private ws: WebSocket;
   private handlers: EventHandlers;
@@ -33,14 +52,19 @@ export default class SimpleSocketIOClient {
   private apiUser: string;
   private apiKey: string;
   private sessionId?: string;
-
   // Added socket object with connected, reconnect and transport properties to match current API
   public socket: {
     connected: boolean;
     reconnect: () => void;
     transport: WebSocket;
   };
-
+  /**
+   * Constructs a new SimpleSocketIOClient instance.
+   * @param serverUrl - The server URL.
+   * @param apiUser - The API user.
+   * @param apiKey - The API key.
+   * @param heartbeatIntervalMs - The heartbeat interval in milliseconds. Defaults to 10000
+   */
   constructor(
     serverUrl: string,
     apiUser: string,
@@ -66,7 +90,11 @@ export default class SimpleSocketIOClient {
     };
     this.initializeWebSocket();
   }
-  // Fetch the session ID from the ftrack server
+  /**
+   * Fetches the session ID from the ftrack server.
+   * @private
+   * @returns A promise that resolves to the session ID.
+   */
   private async fetchSessionId(): Promise<string> {
     try {
       const url = new URL(`${this.serverUrl}/socket.io/1/`);
@@ -97,26 +125,43 @@ export default class SimpleSocketIOClient {
       throw error;
     }
   }
+  /**
+   *Initializes the WebSocket connection.
+   * @private
+   * @returns A promise that waits for the session ID and resolves when the
+   * initial event listeners have been added.
+   */
   private async initializeWebSocket(): Promise<void> {
     const sessionId = this.sessionId ?? (await this.fetchSessionId());
     const urlWithQueryAndSession = `${this.wsUrl}/socket.io/1/websocket/${sessionId}?${this.query}`;
     this.ws = new WebSocket(urlWithQueryAndSession);
     // Set transport property as a public alias of the websocket
     this.socket.transport = this.ws;
-    this.addInitialEventListeners();
+    this.addInitialEventListeners(this.ws);
   }
 
-  private addInitialEventListeners(): void {
-    this.ws?.addEventListener("message", this.handleMessage.bind(this));
-    this.ws?.addEventListener("open", this.handleOpen.bind(this));
-    this.ws?.addEventListener("close", this.handleClose.bind(this));
-    this.ws?.addEventListener("error", this.handleError.bind(this));
+  /**
+   * Adds initial event listeners to the WebSocket
+   * @private
+   */
+  private addInitialEventListeners(ws: WebSocket): void {
+    ws.addEventListener("message", this.handleMessage.bind(this));
+    ws.addEventListener("open", this.handleOpen.bind(this));
+    ws.addEventListener("close", this.handleClose.bind(this));
+    ws.addEventListener("error", this.handleError.bind(this));
   }
-
+  /**
+   * Handles WebSocket errors
+   * @private
+   */
   private handleError(event: Event): void {
     this.handleClose();
     console.error("WebSocket error:", event);
   }
+  /**
+   * Handles WebSocket messages
+   * @private
+   */
   private handleMessage(event: MessageEvent): void {
     const [packetType, data] = event.data.split(/:::?/);
     if (packetType === PACKET_TYPES.event) {
@@ -137,6 +182,10 @@ export default class SimpleSocketIOClient {
       return;
     }
   }
+  /**
+   * Handles WebSocket open event
+   * @private
+   */
   private handleOpen(): void {
     this.startHeartbeat();
     if (this.reconnectTimeout) {
@@ -147,24 +196,43 @@ export default class SimpleSocketIOClient {
     // Set connected property to true
     this.socket.connected = true;
   }
-
+  /**
+   * Handles WebSocket close event
+   * @private
+   */
   private handleClose(): void {
     this.stopHeartbeat();
     this.scheduleReconnect();
     // Set connected property to false
     this.socket.connected = false;
   }
+  /**
+   * Calls all callbacks for the given eventName with the given eventData.
+   * @private
+   * @param eventName - The event name.
+   * @param eventData - The event data.
+   */
   private handleEvent(eventName: string, eventData?: any): void {
     this.handlers[eventName]?.forEach((callback) => callback(eventData));
   }
-  // Setup event callbacks for a given eventName
+  /**
+   * Sets up event callbacks for a given eventName.
+   * @private
+   * @param eventName - The event name.
+   * @param eventCallback - The event callback.
+   */
   public on(eventName: string, eventCallback: (eventData: any) => void): void {
     if (!this.handlers[eventName]) {
       this.handlers[eventName] = [];
     }
     this.handlers[eventName].push(eventCallback);
   }
-  // Emit an event with the given eventName and eventData
+  /**
+   * Emits an event with the given eventName and eventData.
+   * @public
+   * @param eventName - The event name.
+   * @param eventData - The event data.
+   */
   public emit(eventName: string, eventData: Event["_data"]): void {
     const payload = {
       name: eventName,
@@ -173,30 +241,51 @@ export default class SimpleSocketIOClient {
     const dataString = eventData ? `:::${JSON.stringify(payload)}` : "";
     this.ws?.send(`${PACKET_TYPES.event}${dataString}`);
   }
-  // Heartbeat methods, to tell the server to keep the connection alive
+  /**
+   * Starts sending heartbeats to the server to keep the connection alive
+   * @private
+   */
   private startHeartbeat(): void {
     this.heartbeatInterval = setInterval(() => {
       this.ws?.send(`${PACKET_TYPES.heartbeat}::`);
     }, this.heartbeatIntervalMs);
   }
-
+  /**
+   * Stops sending heartbeats to the server
+   * @private
+   */
   private stopHeartbeat(): void {
     if (this.heartbeatInterval) {
       clearInterval(this.heartbeatInterval);
       this.heartbeatInterval = undefined;
     }
   }
-
+  /**
+   * Returns whether the WebSocket is connected or not.
+   * Different from the socket.connected property, which is set
+   * manually. This uses the readyState from the WebSocket.
+   * @public
+   * @returns - True if the WebSocket is open, false otherwise.
+   */
   public isConnected(): boolean {
     return this.ws?.readyState === WebSocket.OPEN;
   }
-  // Reconnect methods, to reconnect to the server if the connection is lost using the same session ID
+  /**
+   *
+   * Reconnects to the server if the connection is lost using the same session ID.
+   * @public
+   */
   public reconnect(): void {
     if (this.socket.connected) {
       this.ws?.close();
     }
     this.initializeWebSocket();
   }
+  /**
+   * Schedules a reconnect attempt after a specified delay.
+   * @private
+   * @param reconnectDelayMs - The delay in milliseconds before attempting to reconnect. Defaults to 5000.
+   */
   private scheduleReconnect(reconnectDelayMs: number = 5000): void {
     if (!this.reconnectTimeout) {
       this.reconnectTimeout = setTimeout(() => {
