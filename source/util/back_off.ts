e3e9a3e1a6b36e2e2873d97243c1bf2fb6dbcb64
Lucas Correia <lucas.correia@ftrack.com>
2023-06-02 16:27:02 +0200

    Add new backOff util

---
+++ source/util/back_off.ts
@@ -0,0 +1,43 @@
+// :copyright: Copyright (c) 2023 ftrack
+import loglevel from "loglevel";
+const logger = loglevel.getLogger("ftrack_api");
+
+const wait = async (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export async function backOff<T>(
+  request: () => Promise<T>,
+  /** Maximum number of attempts to attempt before giving up */
+  maxAttempts = 16,
+  /** Maximum delay between attempts [64 seconds] */
+  maxDelayMs = 64 * 1000,
+  /** Fuzz/Jitter each attempt by [0-fuzzMs] ms */
+  fuzzMs = 500
+) {
+  let attemptNumber = 0;
+  let delayMs = 500;
+  let delayFuzzMs = 0;
+
+  while (attemptNumber <= maxAttempts) {
+    try {
+      return await request();
+    } catch (error) {
+      attemptNumber += 1;
+      if (attemptNumber >= maxAttempts) {
+        logger.warn(`Failed to execute function in ${maxAttempts} attempts.`);
+        throw error;
+      }
+      delayMs = Math.min(maxDelayMs, delayMs * 2);
+      delayFuzzMs = Math.floor(Math.random() * fuzzMs);
+
+      logger.warn(
+        `Failed to execute function, retrying in ${Math.floor(
+          delayMs / 1000
+        )} seconds...`
+      );
+      // eslint-disable-next-line no-loop-func
+      await wait(delayMs + delayFuzzMs);
+    }
+  }
+  throw new Error("Something went wrong");
+}
---
+++ test/back_off.test.js
@@ -0,0 +1,44 @@
+// :copyright: Copyright (c) 2023 ftrack
+import { backOff } from "../source/util/back_off";
+import { expect } from "vitest";
+
+describe("backOff", () => {
+  it("should return the result of the successful request", async () => {
+    let attempts = 0;
+    const successfulRequest = async () => {
+      attempts++;
+      return "Success";
+    };
+    const result = await backOff(successfulRequest, 3, 0, 0);
+    expect(result).toBe("Success");
+    expect(attempts).toBe(1);
+  });
+
+  it("should retry the request and throw an error if all attempts fail", async () => {
+    let attempts = 0;
+    const failingRequest = async () => {
+      attempts += 1;
+      throw new Error("Request failed");
+    };
+
+    await expect(backOff(failingRequest, 3, 0, 0)).rejects.toThrow(
+      "Request failed"
+    );
+    expect(attempts).toBe(3);
+  });
+
+  it("should retry the request and succeed after a failed attempt", async () => {
+    let attempts = 0;
+    const successfulAfterAttempt = async () => {
+      attempts += 1;
+      if (attempts === 1) {
+        throw new Error("First attempt failed");
+      }
+      return "Success";
+    };
+
+    const result = await backOff(successfulAfterAttempt, 3, 0, 0);
+    expect(result).toBe("Success");
+    expect(attempts).toBe(2);
+  });
+});
