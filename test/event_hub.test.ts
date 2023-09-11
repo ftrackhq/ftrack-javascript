6444f869714fbbf1f53768ce14794fb998d2259e
Lars Johansson <lars.johansson@ftrack.com>
2023-09-11 16:54:17 +0200

    Revert "fix: Revert "Replace socket.io-client" (#111)"
    This reverts commit ca25de7922705c4113393511280da06aa7dcc42e.

--- test/event_hub.test.ts
+++ test/event_hub.test.ts
@@ -12,6 +12,7 @@ describe("EventHub", () => {
       emit: vi.fn(),
       socket: { connected: true },
     };
+    eventHub.isConnected = vi.fn(() => true);
   });
 
   afterEach(() => {
@@ -238,4 +239,32 @@ describe("EventHub", () => {
     };
     expect(EventData).toEqual(expectedEvent);
   });
+});
+
+test("EventHub constructor", async () => {
+  // Scenario 1
+  const eventHub1 = new EventHub("https://ftrack.test", "testUser", "testKey", {
+    applicationId: "custom.app.id",
+  });
+
+  // Check instance properties for scenario 1
+  expect(eventHub1._applicationId).toBe("custom.app.id");
+  expect(eventHub1._apiUser).toBe("testUser");
+  expect(eventHub1._apiKey).toBe("testKey");
+  expect(eventHub1._serverUrl).toBe("https://ftrack.test:443");
+  expect(eventHub1.logger).toBeDefined();
+
+  // Scenario 2
+  const eventHub2 = new EventHub(
+    "http://ftrack.test:8080",
+    "testUser",
+    "testKey"
+  );
+
+  // Check instance properties for scenario 2
+  expect(eventHub2._applicationId).toBe("ftrack.api.javascript");
+  expect(eventHub2._apiUser).toBe("testUser");
+  expect(eventHub2._apiKey).toBe("testKey");
+  expect(eventHub2._serverUrl).toBe("http://ftrack.test:8080");
+  expect(eventHub2.logger).toBeDefined();
 });
--- test/server.ts
+++ test/server.ts
@@ -7,7 +7,12 @@ import getUploadMetadata from "./fixtures/get_upload_metadata.json";
 import completeMultipartUpload from "./fixtures/complete_multipart_upload.json";
 import exampleQuery from "./fixtures/query_select_name_from_task_limit_1.json";
 import { setupServer } from "msw/node";
-
+const InvalidCredentialsError = {
+  content:
+    'The supplied API key is not valid. API keys are created from Settings under the page API keys. The api key should be passed in the request header "ftrack-api-key".',
+  exception: "InvalidCredentialsError",
+  error_code: null,
+};
 function authenticate(req) {
   // allow returning invalid authentication by setting ftrack-api-key to "INVALID_API_KEY"
   // otherwise, return true
@@ -36,14 +41,7 @@ export function getExampleQuery() {
 export const handlers = [
   rest.post("http://ftrack.test/api", async (req, res, ctx) => {
     if (!authenticate(req)) {
-      return res(
-        ctx.json({
-          content:
-            'The supplied API key is not valid. API keys are created from Settings under the page API keys. The api key should be passed in the request header "ftrack-api-key".',
-          exception: "InvalidCredentialsError",
-          error_code: null,
-        })
-      );
+      return res(ctx.json(InvalidCredentialsError));
     }
     const body = await Promise.all(
       (
@@ -124,6 +122,23 @@ export const handlers = [
   rest.put("http://ftrack.test/file-url", async (req, res, ctx) => {
     return res(ctx.status(200), ctx.set("Access-Control-Allow-Origin", "*"));
   }),
+  // Get socket io session id
+  rest.get("https://ftrack.test/socket.io/1/", handleSocketIORequest),
+  rest.get("http://ftrack.test/socket.io/1/", handleSocketIORequest),
+  rest.get("https://ftrack.test/*", (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
+  rest.get("http://ftrack.test:8080/*", (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
 ];
+// Get socket io session id
+function handleSocketIORequest(req, res, ctx) {
+  if (!authenticate(req)) {
+    return ctx.json(InvalidCredentialsError);
+  }
+  return res(ctx.text("1234567890:")); // The returned session ID has a colon and then some other information at the end. This only has the colon, to check that the colon is removed.
+}
 
 export const server = setupServer(...handlers);
