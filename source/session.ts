aa4d9da3caf666bf1508a1cadc4307a0cda94353
Mathias Lykkegaard Lorenzen <mathias.lykkegaard.lorenzen@lego.com>
2023-09-12 22:45:09 +0200

    feat(security): add injection prevention (#139)
    * feat(security): add injection prevention
    
    * Update session.ts
    
    * Add tests for template tag
    
    * Add the prepared import to the test
    
    * fix: review changes
    
    * chore: rename `prepared` to `expression`
    
    * fix: conflict fixes
    
    * fix: compile errors
    
    ---------
    
    Co-authored-by: Lars Johansson <gismya@gmail.com>

--- source/session.ts
+++ source/session.ts
@@ -1012,3 +1012,21 @@ export class Session {
     });
   }
 }
+
+/**
+ * Tagged string template for preparing statements to prevent injection attacks.
+ */
+export function expression(
+  literals: TemplateStringsArray,
+  ...placeholders: string[]
+): string {
+  let result = "";
+
+  for (let i = 0; i < placeholders.length; i++) {
+    result += literals[i];
+    result += placeholders[i].replace(/(['"])/g, "\\$1");
+  }
+
+  result += literals[literals.length - 1];
+  return result;
+}
