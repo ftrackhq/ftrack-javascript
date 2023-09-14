6ad868759d83af998fc5934cfb98064efe5c20f2
Lars Johansson <lars.johansson@ftrack.com>
2023-09-14 20:22:28 +0200

    fix: CORS issues with new websocket implementation (#142)
    * Revert "fix: Revert "Replace socket.io-client" (#111)"
    
    This reverts commit ca25de7922705c4113393511280da06aa7dcc42e.
    
    * Fix cors issue on fetchSessionId
    
    * Include credentials in fetch
    
    * Another one
    
    * Improved reconnect behaviour
    
    * Add an online listener to reconnect before reconnectTimeout in case browser online event triggers.
    
    * fixed tests and improved error handling
    
    * Refetch sessionId every connection attempt and bind this to eventlistener callback

--- source/event_hub.ts
+++ source/event_hub.ts
@@ -1,7 +1,7 @@
 // :copyright: Copyright (c) 2016 ftrack
 import { v4 as uuidV4 } from "uuid";
 import loglevel from "loglevel";
-import * as io from "./socket.io-websocket-only.js";
+import io from "./simple_socketio.js";
 import { Event } from "./event.js";
 import {
   EventServerConnectionTimeoutError,
@@ -72,11 +72,11 @@ export type EventPayload =
   | UpdateEventPayload;
 
 export interface EventSource {
-  clientToken: string;
+  clientToken?: string;
   applicationId: string;
   user: {
     username: string;
-    id: string;
+    id?: string;
   };
   id: string;
 }
@@ -132,7 +132,7 @@ export class EventHub {
   };
   private _unsentEvents: ConnectionCallback[];
   private _subscribers: Subscriber[];
-  private _socketIo: io.SocketIO | null;
+  private _socketIo: io | null;
 
   /**
    * Construct EventHub instance with API credentials.
@@ -177,17 +177,7 @@ export class EventHub {
 
   /** Connect to the event server. */
   connect(): void {
-    this._socketIo = io.connect(this._serverUrl, {
-      "max reconnection attempts": Infinity,
-      "reconnection limit": 10000,
-      "reconnection delay": 5000,
-      transports: ["websocket"],
-      query: new URLSearchParams({
-        api_user: this._apiUser,
-        api_key: this._apiKey,
-      }).toString(),
-    });
-
+    this._socketIo = io.connect(this._serverUrl, this._apiUser, this._apiKey);
     this._socketIo.on("connect", this._onSocketConnected);
     this._socketIo.on("ftrack.event", this._handle);
   }
@@ -197,7 +187,7 @@ export class EventHub {
    * @return {Boolean}
    */
   isConnected(): boolean {
-    return (this._socketIo && this._socketIo.socket.connected) || false;
+    return this._socketIo?.isConnected() || false;
   }
 
   /**
@@ -247,7 +237,7 @@ export class EventHub {
    *
    * If timeout is non-zero, the promise will be rejected if the event is not
    * sent before the timeout is reached. Should be specified as seconds and
-   * will default to 10.
+   * will default to 30.
    *
    * @param  {Event}  event               Event instance to publish
    * @param  {Function} [options.onReply] Function to be invoked when a reply
@@ -368,7 +358,7 @@ export class EventHub {
         // Force reconnect socket if not automatically reconnected. This
         // happens for example in Adobe After Effects when rendering a
         // sequence takes longer than ~30s and the JS thread is blocked.
-        this._socketIo.socket.reconnect();
+        this._socketIo.reconnect();
       }
     } else {
       callback();
