0e0f721b27a709ddab17d56e8d500b8ef3c03301
Lucas Stålner Correia <lucas.correia@ftrack.com>
2023-11-21 10:04:12 +0100

    fix: Incomplete date-times are encoded as iso-formatted date-time (#171)

--- source/util/convert_to_iso_string.ts
+++ source/util/convert_to_iso_string.ts
@@ -2,10 +2,16 @@ import { Moment } from "moment";
 import type dayjs from "dayjs";
 
 /**
- * Checks if string is in ISO 6801 format.
+ * Checks if string is in ISO 6801 format including time and optionally
+ * milliseconds and timezone.
+ *
+ * Matches:
+ *   - YYYY-MM-DDThh:mm:ss
+ *   - YYYY-MM-DDThh:mm:ssTZD
+ *   - YYYY-MM-DDThh:mm:ss.sTZD
  */
 function isIsoDate(str: string) {
-  return /^([+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24:?00)([.,]\d+(?!:))?)?(\17[0-5]\d([.,]\d+)?)?([zZ]|([+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/.test(
+  return /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i.test(
     str,
   );
 }
